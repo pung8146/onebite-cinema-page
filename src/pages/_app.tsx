b6173ce09a88b047618941e763551a1d344adca7
pung8146 <pung8146@naver.com>
2025-06-25 22:10:37 +0900

    헤더의 홈 버튼을 링크로 변경하고 라우팅 기능 주석 처리

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,20 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { useRouter } from "next/router";
+import Link from "next/link";
+// import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
+  // const router = useRouter();
 
-  const onClickButtonHome = () => {
-    router.push("/");
-  };
+  // const onClickButtonHome = () => {
+  //   router.push("/");
+  // };
 
   return (
     <>
       <header>
-        <button onClick={onClickButtonHome}>ONEBITE CINEMA</button>
+        {/* <button onClick={onClickButtonHome}>ONEBITE CINEMA</button> */}
+        <Link href="/">ONEBITE CINEMA</Link>
       </header>
       <Component {...pageProps} />;
     </>
