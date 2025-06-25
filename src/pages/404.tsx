5ba01e4e2ee67add8151ca380bb4173b25da4eb7
pung8146 <pung8146@naver.com>
2025-06-25 22:07:35 +0900

    헤더에 홈 버튼 추가 및 라우팅 기능 구현

---
+++ src/pages/404.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <h1>404 페이지</h1>;
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  const onClickButtonHome = () => {
+    router.push("/");
+  };
+
+  return (
+    <>
+      <header>
+        <button onClick={onClickButtonHome}>ONEBITE CINEMA</button>
+      </header>
+      <Component {...pageProps} />;
+    </>
+  );
 }
